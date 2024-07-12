9cb25f3132a4e3519fc8e5cfb836f34baa2fa21c
Sulakshana Karunarathne <131609892+Sulakshana29@users.noreply.github.com>
2024-07-12 15:04:27 +0530

    4_2.scala

--- 4_2.scala
+++ 4_2.scala
@@ -1,14 +1,21 @@
-object numberPattern {
-  def main(args: Array[String]): Unit = {
-    if (args.length != 1){
-      println("Invalid input!")
-    }else{
-      val input = args(0).toInt
-      input match{
-        case x if x <= 0     => println("Negative/Zero")
-        case x if x % 2 == 0 => println("Even number is given")
-        case x if x % 2 != 0 => println("Odd number is given")
-      }
-    }
-  }
+object NameFormat{
+
+    def toUpper(str:String): String = str.toUpperCase()
+    def toLower(str:String): String = str.toLowerCase()
+    def formatNames(name: String)(formatFunc: String => String): String = formatFunc(name)
+
+    def main(args: Array[String]): Unit = {
+    val names = List("Benny", "Niroshan", "Saman", "Kumara")
+
+    def Format1(name: String): String = toUpper(name)
+    def Format2(name: String): String = toUpper(name.substring(0, 2)) + toLower(name.substring(2,8))
+    def Format3(name: String): String = toLower(name)
+    def Format4(name: String): String = toUpper(name.substring(0, 1)) + toLower(name.substring(1, 5)) + toUpper(name.substring(5))
+
+    println(formatNames(names(0))(Format1)) 
+    println(formatNames(names(1))(Format2))  
+    println(formatNames(names(2))(Format3))  
+    println(formatNames(names(3))(Format4))  
+
+}
 }
